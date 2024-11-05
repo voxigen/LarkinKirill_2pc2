715b29e6e667bd147a14a96cee151302574795da
voxi <vox>
2024-11-05 09:22:19 +0300

    sdaafsddasf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,10 +2,16 @@
 {
     public string name { get; set; }
     public int sell { get; set; }
+    public List<string> tags { get; set; }
     public Coin (string n, int s)
     {
         name = n;
         sell = s;
     }
+    public void AddTags (string name)
+    {
+        tags.Add (name);
+    }
+    
 }
 
